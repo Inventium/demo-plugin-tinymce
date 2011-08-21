11b922b9755e641829a15940790038d997fb820c
Dave Doolin <david.doolin@gmail.com>
2011-08-20 20:15:24 -0700

    Started cleaning up by cleaning out old comments and firephp code.

--- demo-plugin-tinymce.php
+++ demo-plugin-tinymce.php
@@ -8,41 +8,6 @@
  Author URI: http://website-in-a-weekend.net/
  */
 
-/*  
- Copyright (c) 2009, David M. Doolin
- http://website-in-a-weekend.net/
- All rights reserved.
- Redistribution and use in source and binary forms, with or without modification,
- are permitted provided that the following conditions are met:
- 
- * Redistributions of source code must retain the above copyright notice,
- this list of conditions and the following disclaimer.
- 
- * Redistributions in binary form must reproduce the above copyright
- notice, this list of conditions and the following disclaimer in the
- documentation and/or other materials provided with the distribution.
- 
- * Neither the name of Website In A Weekend nor the names of its contributors
- may be used to endorse or promote products derived from this software
- without specific prior written permission.
- 
- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
- CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
- PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
- OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
- NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
-
-// FirePHP initialization.
-//require_once ('FirePHPCore/FirePHP.class.php');
-//ob_start();
 
 /* 
  * Add a button to the editor interface.
@@ -56,14 +21,7 @@ global $tinymce_plugin_url;
 $tinymce_plugin_url = WP_PLUGIN_URL.'/demo-plugin-tinymce';
 
 
-//$firephp = FirePHP::getInstance(true);
-//$firephp->log('From top level...', 'From top level...');
-
-
 function tinymce_plugin_init() {
-
-    //$firephp = FirePHP::getInstance(true);
-    //$firephp->log('tinymce_plugin_init', 'tinymce_plugin_init');
     
     //if (get_user_option('rich_editing') == 'true') {
     // Include hooks for TinyMCE plugin
@@ -121,9 +79,6 @@ function tinymce_script_init() {
 }
 
 function tinymce_plugin_mce_external_plugins($plugins) {
-
-    //$firephp = FirePHP::getInstance(true);
-    //$firephp->log('tinymce_plugin_mce_external_plugins', 'tinymce_plugin_mce_external_plugins');
     
     global $tinymce_plugin_url;
     $plugins['demo_tinymce'] = $tinymce_plugin_url.'/tinymceplugin/editor_plugin.js';
@@ -132,9 +87,6 @@ function tinymce_plugin_mce_external_plugins($plugins) {
 
 function tinymce_plugin_mce_buttons($buttons) {
 
-    //$firephp = FirePHP::getInstance(true);
-    //$firephp->log('tinymce_plugin_mce_buttons', 'tinymce_plugin_mce_buttons');
-    
     array_push($buttons, 'demo_tinymce_button');
     return $buttons;
 }
